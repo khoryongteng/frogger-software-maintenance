1248359549fe6d11c5c8c930798d34ae42b9bb7a
YongTeng <49972425+YongTeng@users.noreply.github.com>
2020-11-07 17:26:36 +0800

    Updated to Maven Standard Directory Layout.
    Resource files in p4_group_8_repo package moved to src/main/resources
    p4_group_8_repo package moved to src/main/java
    File path references in Java Files updated to conform to new format

--- src/main/java/p4_group_8_repo/Turtle.java
+++ src/main/java/p4_group_8_repo/Turtle.java
@@ -32,9 +32,9 @@ public class Turtle extends Actor{
 			setX(600);
 	}
 	public Turtle(int xpos, int ypos, int s, int w, int h) {
-		turtle1 = new Image("file:src/p4_group_8_repo/TurtleAnimation1.png", w, h, true, true);
-		turtle2 = new Image("file:src/p4_group_8_repo/TurtleAnimation2.png", w, h, true, true);
-		turtle3 = new Image("file:src/p4_group_8_repo/TurtleAnimation3.png", w, h, true, true);
+		turtle1 = new Image("file:src/main/resources/TurtleAnimation1.png", w, h, true, true);
+		turtle2 = new Image("file:src/main/resources/TurtleAnimation2.png", w, h, true, true);
+		turtle3 = new Image("file:src/main/resources/TurtleAnimation3.png", w, h, true, true);
 		setX(xpos);
 		setY(ypos);
 		speed = s;
