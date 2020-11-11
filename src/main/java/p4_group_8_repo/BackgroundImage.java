7603f5db4f49d5630dee8708088583ddf033cab3
YongTeng <49972425+YongTeng@users.noreply.github.com>
2020-11-11 21:04:59 +0800

    Update BackgroundImage.java
    Reorganized End.java constructor and methods

--- src/main/java/p4_group_8_repo/BackgroundImage.java
+++ src/main/java/p4_group_8_repo/BackgroundImage.java
@@ -4,15 +4,18 @@ import javafx.scene.image.Image;
 
 public class BackgroundImage extends Actor{
 
+	public BackgroundImage(String imageLink) {
+		
+		setImage(new Image(imageLink, 600, 800, true, true));
+		
+	}
+	
 	@Override
 	public void act(long now) {
 		
 		
 	}
 	
-	public BackgroundImage(String imageLink) {
-		setImage(new Image(imageLink, 600, 800, true, true));
-		
-	}
+	
 
 }
