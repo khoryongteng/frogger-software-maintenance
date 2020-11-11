53657c9d31a9696d60417e7b60d279bd729d50a1
YongTeng <49972425+YongTeng@users.noreply.github.com>
2020-11-11 19:42:20 +0800

    Refactored Obstacle.java
    Brokedown methods into smaller components
    Changed argument naming in constructors and methods for readability

--- src/main/java/p4_group_8_repo/Obstacle.java
+++ src/main/java/p4_group_8_repo/Obstacle.java
@@ -4,20 +4,30 @@ import javafx.scene.image.Image;
 
 public class Obstacle extends Actor {
 	private int speed;
+	
+	//Constructor
+	public Obstacle(String imageLink, int x, int y, int speed, int width, int height) {
+		setImage(new Image(imageLink, width, height, true, true));
+		setX(x);
+		setY(y);
+		this.speed = speed;
+	}
+
+	//Action when method called
 	@Override
 	public void act(long now) {
 		move(speed , 0);
+		KeepWithinWindow(speed);
+	}
+	
+	//Keep obstacle within game window borders
+	private void KeepWithinWindow(int speed) {
+		
 		if (getX() > 600 && speed>0)
 			setX(-200);
 		if (getX() < -50 && speed<0)
 			setX(600);
+		
 	}
 	
-	public Obstacle(String imageLink, int xpos, int ypos, int s, int w, int h) {
-		setImage(new Image(imageLink, w,h, true, true));
-		setX(xpos);
-		setY(ypos);
-		speed = s;
-	}
-
 }
