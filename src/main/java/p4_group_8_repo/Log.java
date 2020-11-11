09bf7745a361c64dbc3b3802221dc62196444152
YongTeng <49972425+YongTeng@users.noreply.github.com>
2020-11-11 19:59:16 +0800

    Refactored Log.java
    Brokedown methods into smaller components
    Changed argument naming in constructors and methods for readability

--- src/main/java/p4_group_8_repo/Log.java
+++ src/main/java/p4_group_8_repo/Log.java
@@ -5,22 +5,34 @@ import javafx.scene.image.Image;
 public class Log extends Actor {
 
 	private double speed;
+	
+	//Constructor
+	public Log(String imageLink, int size, int x, int y, double speed) {
+		setImage(new Image(imageLink, size, size, true, true));
+		setX(x);
+		setY(y);
+		this.speed = speed;
+		
+	}
+	
+	//Action when method called
 	@Override
 	public void act(long now) {
 		move(speed , 0);
+		KeepWithinWindow(speed);
+	}
+	
+	//Keep Log within game window borders
+	private void KeepWithinWindow(double speed) {
+		
 		if (getX()>600 && speed>0)
 			setX(-180);
 		if (getX()<-300 && speed<0)
 			setX(700);
-	}
-	
-	public Log(String imageLink, int size, int xpos, int ypos, double s) {
-		setImage(new Image(imageLink, size,size, true, true));
-		setX(xpos);
-		setY(ypos);
-		speed = s;
 		
 	}
+	
+	//Check if log is moving left
 	public boolean getLeft() {
 		return speed < 0;
 	}
