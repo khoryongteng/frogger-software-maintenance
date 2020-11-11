bb5edf73fa90b561ca6849089e2fdad089428ff6
YongTeng <49972425+YongTeng@users.noreply.github.com>
2020-11-11 20:47:47 +0800

    Update Digit.java

--- src/main/java/p4_group_8_repo/Digit.java
+++ src/main/java/p4_group_8_repo/Digit.java
@@ -5,17 +5,19 @@ import javafx.scene.image.Image;
 public class Digit extends Actor{
 	int dimensions;
 	Image image1;
-	@Override
-	public void act(long now) {
-		// TODO Auto-generated method stub
-		
-	}
 	
+	//constructor
 	public Digit(int n, int dimensions, int x, int y) {
 		image1 = new Image("file:src/main/resources/"+n+".png", dimensions, dimensions, true, true);
 		setImage(image1);
 		setX(x);
 		setY(y);
+	}
+	
+	@Override
+	public void act(long now) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
