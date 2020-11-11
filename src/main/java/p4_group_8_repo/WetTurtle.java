11cc9c89b2fe432b0558155c708c525862e24863
YongTeng <49972425+YongTeng@users.noreply.github.com>
2020-11-11 16:07:10 +0800

    Refactored WetTurtle.java
    WetTurtle
    Brokedown methods into smaller components
    Removed unused variables
    Changed argument naming in constructors and methods for readability
    Changed variable name for readability

--- src/main/java/p4_group_8_repo/WetTurtle.java
+++ src/main/java/p4_group_8_repo/WetTurtle.java
@@ -3,51 +3,73 @@ package p4_group_8_repo;
 import javafx.scene.image.Image;
 
 public class WetTurtle extends Actor{
-	Image turtle1;
-	Image turtle2;
-	Image turtle3;
-	Image turtle4;
+	Image turtleframe1;
+	Image turtleframe2;
+	Image turtleframe3;
+	Image turtleframe4;
 	private int speed;
-	int i = 1;
-	boolean bool = true;
 	boolean sunk = false;
+	
+	//Constructor
+	public WetTurtle(int x, int y, int speed, int width, int height) {
+		
+		turtleframe1 = new Image("file:src/main/resources/TurtleAnimation1.png", width, height, true, true);
+		turtleframe2 = new Image("file:src/main/resources/TurtleAnimation2Wet.png", width, height, true, true);
+		turtleframe3 = new Image("file:src/main/resources/TurtleAnimation3Wet.png", width, height, true, true);
+		turtleframe4 = new Image("file:src/main/resources/TurtleAnimation4Wet.png", width, height, true, true);
+		setX(x);
+		setY(y);
+		this.speed = speed;
+		setImage(turtleframe2);
+		
+	}
+	
+	//Action when method called
 	@Override
 	public void act(long now) {
 
-				if (now/900000000  % 4 ==0) {
-					setImage(turtle2);
-					sunk = false;
-					
-				}
-				else if (now/900000000 % 4 == 1) {
-					setImage(turtle1);
-					sunk = false;
-				}
-				else if (now/900000000 %4 == 2) {
-					setImage(turtle3);
-					sunk = false;
-				} else if (now/900000000 %4 == 3) {
-					setImage(turtle4);
-					sunk = true;
-				}
-			
+		TurtleState(now);
 		move(speed , 0);
+		KeepWithinWindow(speed);
+		
+	}
+	
+	//TurtleAnimation by set image
+	private void TurtleState(long now) {
+			
+		if (now/900000000  % 4 ==0) {
+			setImage(turtleframe2);
+			sunk = false;
+			
+		}
+		else if (now/900000000 % 4 == 1) {
+			setImage(turtleframe1);
+			sunk = false;
+		}
+		else if (now/900000000 %4 == 2) {
+			setImage(turtleframe3);
+			sunk = false;
+		} else if (now/900000000 %4 == 3) {
+			setImage(turtleframe4);
+			sunk = true;
+		}
+			
+	}
+		
+	//Keep turtle within game window borders
+	private void KeepWithinWindow(int speed) {
+			
 		if (getX() > 600 && speed>0)
 			setX(-200);
 		if (getX() < -75 && speed<0)
 			setX(600);
+		
 	}
-	public WetTurtle(int xpos, int ypos, int s, int w, int h) {
-		turtle1 = new Image("file:src/main/resources/TurtleAnimation1.png", w, h, true, true);
-		turtle2 = new Image("file:src/main/resources/TurtleAnimation2Wet.png", w, h, true, true);
-		turtle3 = new Image("file:src/main/resources/TurtleAnimation3Wet.png", w, h, true, true);
-		turtle4 = new Image("file:src/main/resources/TurtleAnimation4Wet.png", w, h, true, true);
-		setX(xpos);
-		setY(ypos);
-		speed = s;
-		setImage(turtle2);
-	}
+	
+	//Return if turtle is sunk
 	public boolean isSunk() {
 		return sunk;
 	}
+	
+	
 }
