1d42bf7106eedb818de8b28c30a8db24a354ef5a
YongTeng <49972425+YongTeng@users.noreply.github.com>
2020-11-11 14:40:41 +0800

    Refactored Turtle.java
    Brokedown methods into smaller components
    Remove unused variables
    Changed argument naming in constructors and methods for readability
    Changed variable name for readability

--- src/main/java/p4_group_8_repo/Turtle.java
+++ src/main/java/p4_group_8_repo/Turtle.java
@@ -3,41 +3,58 @@ package p4_group_8_repo;
 import javafx.scene.image.Image;
 
 public class Turtle extends Actor{
-	Image turtle1;
-	Image turtle2;
-	Image turtle3;
+	Image turtleframe1;
+	Image turtleframe2;
+	Image turtleframe3;
 	private int speed;
-	int i = 1;
-	boolean bool = true;
+	
+	//Constructor
+	public Turtle(int x, int y, int speed, int width, int height) {
+		turtleframe1 = new Image("file:src/main/resources/TurtleAnimation1.png", width, height, true, true);
+		turtleframe2 = new Image("file:src/main/resources/TurtleAnimation2.png", width, height, true, true);
+		turtleframe3 = new Image("file:src/main/resources/TurtleAnimation3.png", width, height, true, true);
+		setX(x);
+		setY(y);
+		this.speed = speed;
+		setImage(turtleframe2);
+	}
+	
+	//Action when method called
 	@Override
 	public void act(long now) {
 
-				if (now/900000000  % 3 ==0) {
-					setImage(turtle2);
-					
-				}
-				else if (now/900000000 % 3 == 1) {
-					setImage(turtle1);
-					
-				}
-				else if (now/900000000 %3 == 2) {
-					setImage(turtle3);
-					
-				}
-			
+		TurtleAnimation(now);	
 		move(speed , 0);
+		KeepWithinWindow(speed);
+		
+	}
+	
+	//TurtleAnimation by set image
+	private void TurtleAnimation(long now) {
+		
+		if (now/900000000  % 3 ==0) {
+			setImage(turtleframe2);
+			
+		}
+		else if (now/900000000 % 3 == 1) {
+			setImage(turtleframe1);
+			
+		}
+		else if (now/900000000 %3 == 2) {
+			setImage(turtleframe3);
+			
+		}
+		
+	}
+	
+	//Keep turtle within game window borders
+	private void KeepWithinWindow(int speed) {
+		
 		if (getX() > 600 && speed>0)
 			setX(-200);
 		if (getX() < -75 && speed<0)
 			setX(600);
+		
 	}
-	public Turtle(int xpos, int ypos, int s, int w, int h) {
-		turtle1 = new Image("file:src/main/resources/TurtleAnimation1.png", w, h, true, true);
-		turtle2 = new Image("file:src/main/resources/TurtleAnimation2.png", w, h, true, true);
-		turtle3 = new Image("file:src/main/resources/TurtleAnimation3.png", w, h, true, true);
-		setX(xpos);
-		setY(ypos);
-		speed = s;
-		setImage(turtle2);
-	}
+	
 }
